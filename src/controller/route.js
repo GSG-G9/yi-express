80da488ace9c640f050d167e11826647421e768d
Israa91 <eng.israa.1991@gmail.com>
2020-12-01 20:48:56 +0200

    write the recieving data from Api to weather file Relate #3

--- src/controller/route.js
+++ src/controller/route.js
@@ -7,22 +7,37 @@ const getMain = (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 };
 
+const writeFile = (filePath, fileContent) => new Promise((resolve, reject) => {
+  fs.writeFile(filePath, fileContent, (err) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(filePath);
+  });
+});
+
 const getWeather = (req, res) => {
-    console.log('hi')
-    console.log(req.body.city)
+  console.log('hi');
+  console.log(req.body.city);
   const createWeatherUrl = (city) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-    console.log(url)
-    return url; 
+    console.log(url);
+    return url;
   };
-  
+
   fetch(createWeatherUrl(req.body.city))
     .then((response) => response.json())
-    .then((data) =>{ 
-    // const stream = fs.createWriteStream(path.join(__dirname,'..','weather.json'))
-    // req.pipe(stream);
-    res.json(data)
-});
-//   res.redirect('/'); 
+    .then((data) => {
+    //   fs.writeFile(path.join(__dirname, '..', 'weather.json'), JSON.stringify(data), (err) => {
+      //     console.log(err);
+      //   });
+      writeFile(path.join(__dirname, '..', 'weather.json'), JSON.stringify(data))
+      // .then(data=>console.log(data))
+        .catch((err) => console.log(err));
+      res.json(data);
+    })
+    .catch((err) => console.log(err));
+//   res.redirect('/');
 };
 module.exports = { getMain, getWeather };
--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ const app = require('./app.js');
 app.listen(app.get('port'), () => {
   console.log('App running on port', app.get('port'));
 });
-  
